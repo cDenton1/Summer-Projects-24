a33eb5c279d4e3ff1221d55c9a0f52d63c11f2a4
Denton <163344926+cDenton1@users.noreply.github.com>
2024-05-25 11:20:13 -0600

    Update mailboxPerms.ps1

--- programming/mailboxPerms.ps1
+++ programming/mailboxPerms.ps1
@@ -1,1 +1,24 @@
-add code here
+# automation file for taking in names from a csv file and giving all their emails "Send As" permissions
+# written by me witha little bit of help from one of my colleagues
+
+$csvPath = Read-Host "Enter path for csv file"
+$csvFile = Import-Csv -Path $csvPath -Delimiter ","
+
+Write-Host "You will be running on these names: "
+($csvFile[0..9].'Display Name') # not sure where the closing bracket should actually be
+Write-Host "..."
+
+$cont = Read-Host "Would you like to continue (y/n)"
+if("y" -eq $cont) {
+  Import-Csv -Path $csvPath -Delimiter "," | ForEach-Object {
+    if("#N/A", $null -eq $_.'Display Name') {}
+    else {
+      # testing command - Write-Host "testing: $_.'Display Name' and $_.PrimarySmtp"
+      Add-ADPermission -Identity $_.'Display Name' -User $_.PrimarySmtp -AccessRights ExtendedRights -ExtendedRights "Send As"
+    }
+  }
+} 
+else {
+  Write-Host "Quit execution, exiting"
+  exit
+}
